6cad5c06217cf50817d36fa6645c1b0027cbd4b6
leesuyeon <lgh0385hh@gmail.com>
2024-09-10 19:59:36 +0900

    FEAT: DAY2

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
 };
 
 export default nextConfig;
+
+// 넥스트의 설정을 관리
+// reactStrictMode는 기존 리액트와 같이 두 번 렌더링하도록 하기에 false로 설정
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  // 기존의 index.html의 역할을 하는 컴포넌트
+  // 메타 태그, 폰트, 서드파티 스크립트 등의
+  // 페이저 전체에 해당하는 HTML
   return (
     <Html lang="en">
       <Head />
