c182fe281146c68951c2e6867c791c7c58934fde
leesuyeon <lgh0385hh@gmail.com>
2024-09-12 23:26:00 +0900

    FIX: 파일 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,33 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.prefetch("/test");
+  }, [])
+
+  const onClickButton = () => {
+    router.push("/test");
+  }  
+
   return (
     <>
+    <header>
+      <Link href={"/"}>&nbsp;root</Link>
+      <Link href={"/search"}>&nbsp;search</Link>
+      <Link href={"/book/1"}>&nbsp;book</Link>
+      <div>
+        <button onClick={onClickButton}>/test 페이지로 이동</button>
+      </div>
+    </header>
     <Component {...pageProps} />
     </>
   )
 }
+
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,13 @@
+//  css module 사용해야 한다
+import style from "./index.module.css";
+
 export default function Home() {
-  return <h1>ONEBITE CINEMA</h1>;
+  return (
+    <>
+    <h1 className={style.h1}>ONEBITE CINEMA</h1>
+    <h2 className={style.h2}>H2</h2>  
+    </>
+  )
+
   
 }
